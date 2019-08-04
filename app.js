a59c305640e576a3d245e0df17c6535c69377b7f
Ryan Pedersen <ryanpedersen42@gmail.com>
2019-08-04 15:46:59 -0700

    removed spaces

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.post('/', (req, res) => { 
-  let text = req.body.text; 
   let responses = [
     { 
     response_type: 'in_channel', 
@@ -76,4 +75,4 @@ app.post('/', (req, res) => {
   res.json(responses[randomNumber]);  
 });
 
-const server = app.listen(3000, () => { console.log('Express server   listening on port %d in %s mode', server.address().port,   app.settings.env);});
+const server = app.listen(3000, () => { console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);});
